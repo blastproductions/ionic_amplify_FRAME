c72ed35fc38f35abe652192d364c93b510c77515
Jonathan Forest <info@productionsblast.com>
2021-02-01 19:54:37 +0800

    Removed crypto warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,45 +4,25 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { APIService } from './API.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  todos: Array<any>;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private apiService: APIService
+    private statusBar: StatusBar
   ) {
     this.initializeApp();
-  }
-
-  createTodo() {
-    this.apiService.CreateTodo({
-        name: 'ionic',
-        description: 'testing'
-    });
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-
-      this.apiService.ListTodos().then((evt) => {
-        this.todos = evt.items;
-      });
-
-      this.apiService.OnCreateTodoListener.subscribe((evt) => {
-        const data = (evt as any).value.data.onCreateTodo;
-        this.todos = [...this.todos, data];
-      });
             
     });
   }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APIService } from '../API.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  todos: Array<any>;
 
+  constructor(
+    private apiService: APIService
+  	) {
+     this.initializeApp(); 	
+  	}
+
+  createTodo() {
+    this.apiService.CreateTodo({
+        name: 'Jo',
+        description: 'New Data'
+    });
+  }
+
+  initializeApp() {
+
+      this.apiService.ListTodos().then((evt) => {
+        this.todos = evt.items;
+      });
+
+      this.apiService.OnCreateTodoListener.subscribe((evt) => {
+        const data = (evt as any).value.data.onCreateTodo;
+        this.todos = [...this.todos, data];
+      });
+      
+  }
 }
